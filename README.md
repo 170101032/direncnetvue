7f4b416e4d28396dab7c1696aa182293a6c39d1a
170101032 <caxania@gmail.com>
2020-11-08 06:24:40 +0300

    Buefy eklendi, temel header eklendi

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # direncnetvue
+
+buefy referans
+https://buefy.org/documentation/field
+
+
+bulma referans
+https://bulma.io/documentation/helpers/spacing-helpers/
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="columns is-multiline">
+        <div class="column is-12 headerTop">
+            <div class="columns">
+                <div class="column is-offset-8 is-pulled-right">
+                    <span class=""><a>Facebook Ile Baglan</a></span>
+                    <span class=""><a>Uye Girisi</a></span>
+                    <span class=""><a>Yeni Uyelik</a></span>
+                    <span class=""><a>TR</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="column is-12 header">
+            <div class="columns has-text-centered">
+                <div class="column">
+                    <img src="@/assets/logo.png" />
+                </div>
+                <div class="column">
+                    <b-field position="is-centered">
+                        <b-input placeholder="Aradiginiz urunun adini yaziniz." type="search" expanded></b-input>
+                        <p class="control">
+                            <b-button class="button">Ara</b-button>
+                        </p>
+                    </b-field>
+                </div>
+                <div class="column">
+                    <b-field position="is-centered">
+                        <p class="control">
+                            <b-button></b-button>
+                        </p>
+                        <p>
+                            0 Urun/0,00TL
+                        </p>
+                    </b-field>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//import store from "@/store";
+
+export default {
+    name: "Header",
+    components: {},
+    props: {},
+    data() {
+        return {};
+    },
+    async created() {},
+    methods: {},
+    watch: {},
+};
+</script>
+
+<style scoped>
+.headerTop {
+    background-color: #1eaef6;
+}
+.headerTop a {
+    font-size: 13px;
+    color: #fff;
+}
+.header {
+    background-color: #0aa2ee;
+}
+
+</style>
